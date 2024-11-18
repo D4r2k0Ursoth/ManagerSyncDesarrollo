77dc29b85edd5903dc707b48ff00891e78111cc7
D4r2k0Ursoth <147214204+D4r2k0Ursoth@users.noreply.github.com>
2024-11-17 23:31:54 -0600

    ............

--- src/components/activity/Registro.jsx
+++ src/components/activity/Registro.jsx
@@ -66,39 +66,37 @@ const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     if (formData.password !== formData.password_confirmation) {
       setErrors({ password_confirmation: 'Las contraseñas no coinciden' });
       return;
     }
-  
+
     const formDataToSend = new FormData();
     for (const key in formData) {
-      // Asegúrate de renombrar 'image' a 'profile_image'
       formDataToSend.append(key === 'image' ? 'profile_image' : key, formData[key]);
     }
-  
+
     try {
       const response = await fetch('https://manaercynbdf-miccs.ondigitalocean.app/api/register', {
         method: 'POST',
-        body: formDataToSend,
+        body: formDataToSend
       });
-  
+
       if (!response.ok) {
         const data = await response.json();
         setErrors(data.errors || {});
         return;
       }
-  
+
       setSuccess('Usuario registrado correctamente.');
       setTimeout(() => {
         navigate('/LogIn');
       }, 2000);
     } catch (error) {
-      console.error('Error al registrar:', error.message);
+      console.error('Error:', error.message);
     }
   };
-  
 
 
 
