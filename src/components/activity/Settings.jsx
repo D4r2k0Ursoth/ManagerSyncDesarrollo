d65ea3a230f3a2f12250a2ba1c24ee717907330f
D4r2k0Ursoth <147214204+D4r2k0Ursoth@users.noreply.github.com>
2024-11-24 06:55:05 -0600

    juju

--- src/components/activity/Settings.jsx
+++ src/components/activity/Settings.jsx
@@ -29,18 +29,6 @@ export function Settings() {
     setEditMode(false);
   };
 
-  const handleDeleteAccount = async () => {
-    const confirmDelete = window.confirm('¿Estás seguro de que deseas eliminar tu cuenta?');
-    if (confirmDelete) {
-      try {
-        await deleteAccount(token); // Aquí se llama a la función para eliminar la cuenta
-        logout(); // Cerrar sesión después de eliminar la cuenta
-      } catch (error) {
-        console.error('Error al eliminar cuenta:', error);
-      }
-    }
-  };
-
   if (!user) {
     return <div>No hay usuario</div>; // Manejo de usuarios no autenticados
   }
@@ -59,96 +47,172 @@ export function Settings() {
           <Sidebar logout={logout} />
         </div>
 
-        <div className="lg:basis-2/4 flex-grow lg:flex-none py-6 px-8 mt-12 w-full bg-white rounded-lg shadow-lg">
-          <h1 className="text-3xl font-bold text-gray-800 mb-6">Configuración de Cuenta</h1>
-          
-          <form onSubmit={handleUpdateProfile} className="space-y-4">
-            <div className="mb-4">
-              <label className="block text-gray-700 font-semibold">Nombre</label>
-              <input
-                type="text"
-                name="nombre"
-                className="w-full mt-1 p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-sky-700"
-                value={formData.nombre}
-                onChange={handleChange}
-                required
-                disabled={!editMode}
-              />
-            </div>
+        <div className="lg:basis-2/4 flex-grow lg:flex-none py-2 pt-12 p-6 mx-auto mt-6 mr-14 lg:mr-0 lg:ml-5 mb-4 h-min bg-white rounded-lg shadow-lg">
+          <h1 className="font-bold lg:text-5xl lg:p-10 p-5 text-4xl lg:mt-0 -mt-6 lg:text-left text-center">Hola, {user.nombre}</h1>
+          <h2 className="font-semibold lg:indent-6 lg:text-2xl text-3xl lg:text-left text-center lg:ml-2 lg:p-2 lg:-mt-9 p-1">Rol: {user.role}</h2>
 
-            <div className="mb-4">
-              <label className="block text-gray-700 font-semibold">Email</label>
-              <input
-                type="email"
-                name="email"
-                className="w-full mt-1 p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-sky-700"
-                value={formData.email}
-                onChange={handleChange}
-                required
-                disabled={!editMode}
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700 font-semibold">Imagen de Perfil</label>
-              <input
-                type="file"
-                name="profile_image"
-                className="w-full mt-1 p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-sky-700"
-                onChange={(e) => setProfileImage(e.target.files[0])}
-                disabled={!editMode}
-              />
-              {profileImageUrl && <img src={profileImageUrl} alt="Profile" className="mt-4 w-32 h-32 rounded-full" />}
-            </div>
-
-            <div className="flex gap-4">
-              <button
-                type="submit"
-                className="w-1/2 mt-4 font-medium text-white bg-sky-900 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-blue-200 p-2 rounded-md shadow-sm"
-                disabled={!editMode}
-              >
-                Actualizar Perfil
-              </button>
-
-              <button
-                type="button"
-                onClick={() => setEditMode(!editMode)}
-                className="w-1/2 mt-4 font-medium text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-200 p-2 rounded-md shadow-sm"
-              >
-                {editMode ? 'Cancelar' : 'Editar Perfil'}
-              </button>
-            </div>
-          </form>
-
-          <div className="mt-6">
-            <button
-              onClick={() => setShowConfirmDelete(true)}
-              className="text-red-600 font-medium text-sm hover:text-red-800"
-            >
-              Eliminar Cuenta
-            </button>
-
-            {showConfirmDelete && (
-              <div className="mt-4">
-                <p className="text-red-600">¡Advertencia! Esta acción es irreversible.</p>
-                <button
-                  onClick={handleDeleteAccount}
-                  className="mt-4 font-medium text-white bg-red-600 hover:bg-red-800 p-2 rounded-md shadow-sm"
+          <div className="lg:grid lg:grid-cols-2 lg:p-10 lg:mt-0 mt-4 mx-12">
+            <div className="relative w-32 h-32 lg:ml-14 overflow-hidden bg-gray-100 rounded-full drop-shadow">
+              {profileImageUrl ? (
+                <img
+                  src={profileImageUrl}
+                  alt="Profile"
+                  className="w-full h-full object-cover p-10"
+                />
+              ) : (
+                <svg
+                  className="absolute w-36 h-36 text-gray-400 -left-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  Confirmar Eliminación
-                </button>
+                  <path
+                    fillRule="evenodd"
+                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              )}
+            </div>
+
+            <div className="grid grid-cols-2 lg:gap-2 gap-6 py-4 lg:py-2">
+              <div className="flex lg:flex-col lg:justify-end ">
                 <button
-                  onClick={() => setShowConfirmDelete(false)}
-                  className="ml-4 font-medium text-gray-600 hover:text-gray-800 p-2 rounded-md"
-                >
-                  Cancelar
+                  onClick={() => setEditMode(!editMode)}
+                  className="w-36 lg:py-2.5 py-0.5 px-5 font-medium -ml-10 text-sm font-mediumfocus:outline-none rounded-full border 
+                  bg-gray-50 text-gray-600 hover:bg-slate-200 hover:text-sky-800">
+                  {editMode ? 'Cancelar' : 'Editar perfil'}
                 </button>
               </div>
-            )}
+
+              <div className="flex lg:flex-col lg:justify-end">
+                <button
+                  onClick={() => setShowConfirmDelete(true)}
+                  className="w-36 lg:py-2.5 lg:px-5 py-0.5 px-7 text-sm font-medium rounded-full border 
+                  bg-gray-50 text-gray-600 hover:bg-slate-200 hover:text-sky-800">
+                  Eliminar cuenta
+                </button>
+              </div>
+            </div>
           </div>
+
+          {editMode && (
+            <form onSubmit={handleUpdateProfile} className="p-10">
+              <div className="lg:grid lg:grid-cols-2 lg:gap-4">
+                <div className='mb-6 lg:mb-0'>
+                  <label className="block text-gray-700 mb-2">Nombre</label>
+                  <input
+                    type="text"
+                    name="nombre"
+                    value={formData.nombre}
+                    onChange={handleChange}
+                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-sky-700"
+                    required
+                  />
+                </div>
+                <div className='mb-6 lg:mb-0'>
+                  <label className="block text-gray-700 mb-2">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-sky-700"
+                    required
+                  />
+                </div>
+                <div className='mb-6 lg:mb-0'>
+                  <label className="block text-gray-700 mb-2">Cédula</label>
+                  <input
+                    type="text"
+                    name="cedula"
+                    value={formData.cedula}
+                    onChange={handleChange}
+                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-sky-700"
+                    required
+                  />
+                </div>
+                <div className='mb-6 lg:mb-0'>
+                  <label className="block text-gray-700">Contraseña actual</label>
+                  <input
+                    type="password"
+                    name="current_password"
+                    value={formData.current_password}
+                    onChange={handleChange}
+                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-sky-700"
+                  />
+                </div>
+                <div className='mb-6 lg:mb-0'>
+                  <label className="block text-gray-700">Nueva contraseña</label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-sky-700"
+                  />
+                </div>
+                <div className='mb-6 lg:mb-0'>
+                  <label className="block text-gray-700">Confirmar nueva contraseña</label>
+                  <input
+                    type="password"
+                    name="password_confirmation"
+                    value={formData.password_confirmation}
+                    onChange={handleChange}
+                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-sky-700"
+                  />
+                </div>
+              
+                <div>
+                  <label className="block text-gray-700 mb-2">Imagen de perfil</label>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => setProfileImage(e.target.files[0])}
+                    className="w-full p-2 rounded lg:ml-0 -ml-4
+                      file:lg:mr-4 file:lg:py-2 file:py-1 file:lg:px-4 file:px-2
+                      file:rounded-full file:border-0
+                      file:text-sm file:font-semibold
+                    file:bg-sky-50 file:text-sky-800
+                    hover:file:bg-sky-100"
+                  />
+                </div>
+              </div>
+              {error && <p className="text-pink-700">{error}</p>}
+              {success && <p className="text-cyan-600">{success}</p>}
+              <div className="flex gap-2 justify-center mt-5">
+                <button type="submit" className="p-2 rounded-full text-white bg-blue-600">Guardar cambios</button>
+              </div>
+            </form>
+          )}
         </div>
       </div>
 
+      {showConfirmDelete && (
+  <div className="modal">
+    <div className="modal-content">
+      <h3>¿Estás seguro de eliminar tu cuenta?</h3>
+      <div className="flex gap-2 justify-center">
+        <button
+          onClick={() => {
+            deleteAccount(token); // Pasar el token aquí
+            setShowConfirmDelete(false);
+          }}
+          className="p-2 rounded-full text-white bg-red-600"
+        >
+          Eliminar cuenta
+        </button>
+        <button
+          onClick={() => setShowConfirmDelete(false)}
+          className="p-2 rounded-full text-white bg-gray-600"
+        >
+          Cancelar
+        </button>
+      </div>
+    </div>
+  </div>
+)}
+      
       <Footer />
     </>
   );
